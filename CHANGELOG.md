5f40c736f37297b5c10d0b0be229b49436e51a85
igorfs10 <igorfs10@gmail.com>
2020-12-31 19:14:51 -0300

    mudanca de pacote de cor do terminal

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,10 +4,11 @@ Lista de alterações do projeto.
 Esse formato é baseado no [Keep a Changelog](https://keepachangelog.com/pt/1.0.0/),
 e esse projeto adere à [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Não lançado]
+## [1.2.1] - 2020-12-31
 ### Modificado
 - Atualização da lib rayon, chrono e blake3
 - Aplicação de otimização opt-level3 para dependências
+- Usa a lib console no lugar da colour para corrigir falhas do console
 
 ## [1.2.0] - 2020-09-27
 ### Adicionado
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 use std::env;
+use std::fs::{create_dir_all, read_to_string, File};
 use std::io;
 use std::io::prelude::*;
-use std::fs::{ File, create_dir_all, read_to_string };
 use std::path::Path;
 
-use rayon::prelude::*;
 use chrono::prelude::*;
-use serde::{ Serialize, Deserialize };
-use photon_rs::native::{ open_image, save_image };
-use photon_rs::transform;
+use console::style;
 use photon_rs::channels;
-use blake3;
-use colour::*;
+use photon_rs::native::{open_image, save_image};
+use photon_rs::transform;
+use rayon::prelude::*;
+use serde::{Deserialize, Serialize};
 
-const ARQUIVO_CONFIGURACAO: &str = "/image_manip_config.toml";   // Nome do arquivo de configurações
-const PASTA_CONVERSAO: &str = "/image_manip_convert";            // Nome da pasta que será criada para colocar as imagens alteradas
+const ARQUIVO_CONFIGURACAO: &str = "/image_manip_config.toml"; // Nome do arquivo de configurações
+const PASTA_CONVERSAO: &str = "/image_manip_convert"; // Nome da pasta que será criada para colocar as imagens alteradas
 
 fn main() {
     // Carrega os argumentos enviados por linha de comando
@@ -30,61 +29,88 @@ fn main() {
     let configuracoes: Config;
 
     /* Verifica se o arquivo existe. Se existir carrega na variável configurações,
-        caso contrário cria um arquivo com a configuração padrão. */
+    caso contrário cria um arquivo com a configuração padrão. */
     if Path::new(&caminho_configuracao).exists() {
-        yellow_ln!("Arquivo de configurações encontrado. Carregando configurações...");
+        println!(
+            "{}",
+            style("Arquivo de configurações encontrado. Carregando configurações...").yellow()
+        );
 
-        let arquivo = read_to_string(&caminho_configuracao).expect("Não foi possível ler o arquivo de configuração.");
-        
+        let arquivo = read_to_string(&caminho_configuracao)
+            .expect("Não foi possível ler o arquivo de configuração.");
+
         // Tentar ler o arquivo de configuração em string e converte para o objeto em caso de erro interrompe a aplicação
         match toml::from_str(&arquivo) {
             Ok(arquivo_convertido) => {
                 configuracoes = arquivo_convertido;
-                green_ln!("Configurações carregadas.");
+                println!("{}", style("Configurações carregadas.").green());
             }
             Err(_) => {
-                yellow_ln!("Não foi possível carregar o arquivo de configurações, criando um arquivo novo...");
+                println!("{}", style("Não foi possível carregar o arquivo de configurações, criando um arquivo novo...").yellow());
                 configuracoes = criar_configuracoes(&caminho_configuracao);
             }
         }
     } else {
-        yellow_ln!("Arquivo de configurações não encontrado. Criando arquivo de configurações novo...");
+        println!(
+            "{}",
+            style(
+                "Arquivo de configurações não encontrado. Criando arquivo de configurações novo..."
+            )
+            .yellow()
+        );
 
         /* Cria o objeto de configuração padrão, converte o objeto em string,
-            cria o arquivo e salva as configurações transformando a string em bytes */
+        cria o arquivo e salva as configurações transformando a string em bytes */
         configuracoes = criar_configuracoes(&caminho_configuracao);
     }
 
     println!("\n");
 
     // Verifica se a pasta de conversão existe e automaticamente cria, caso não exista
-    match create_dir_all(&caminho_conversao){
+    match create_dir_all(&caminho_conversao) {
         Ok(_) => {
             // Percorre a array de caminho das imagens
-            lista_imagens.par_iter()
-            .for_each(|caminho_imagem| {
-                yellow_ln!("Alterando imagem {}...", caminho_imagem);
+            lista_imagens.par_iter().for_each(|caminho_imagem| {
+                println!(
+                    "{}",
+                    style(format!("Alterando imagem {}...", caminho_imagem)).yellow()
+                );
 
                 // Abre a imagem recebida por parâmetro
-                let mut img = open_image(caminho_imagem);
-                
+                let mut img = open_image(caminho_imagem).expect("Arquivo inválido.");
+
                 // Retorna uma nova imagem com dimensões alteradas se as medidas definidas forem diferentes de 0
                 if configuracoes.largura != 0 && configuracoes.altura != 0 {
-                    img = transform::resize(&img, configuracoes.largura, configuracoes.altura, transform::SamplingFilter::Lanczos3);
+                    img = transform::resize(
+                        &img,
+                        configuracoes.largura,
+                        configuracoes.altura,
+                        transform::SamplingFilter::Lanczos3,
+                    );
                 }
 
                 // Altera os canais da imagem se definidas
-                if configuracoes.canal_vermelho != 0 { channels::alter_blue_channel(&mut img, configuracoes.canal_vermelho); }
-                if configuracoes.canal_verde != 0 { channels::alter_green_channel(&mut img, configuracoes.canal_verde); }
-                if configuracoes.canal_azul != 0 { channels::alter_red_channel(&mut img, configuracoes.canal_azul); }
+                if configuracoes.canal_vermelho != 0 {
+                    channels::alter_blue_channel(&mut img, configuracoes.canal_vermelho);
+                }
+                if configuracoes.canal_verde != 0 {
+                    channels::alter_green_channel(&mut img, configuracoes.canal_verde);
+                }
+                if configuracoes.canal_azul != 0 {
+                    channels::alter_red_channel(&mut img, configuracoes.canal_azul);
+                }
 
                 // Rotaciona a imagem se for setado
-                if configuracoes.espelhamento_horizontal { transform::fliph(&mut img); }
-                if configuracoes.espelhamento_vertical { transform::flipv(&mut img); }
+                if configuracoes.espelhamento_horizontal {
+                    transform::fliph(&mut img);
+                }
+                if configuracoes.espelhamento_vertical {
+                    transform::flipv(&mut img);
+                }
 
                 // Pega a hora para gerar um nome de arquivo único
                 let data: DateTime<Utc> = Utc::now();
-                
+
                 // Cria uma chave usando a hora atual e o caminho total do arquivo para criar a hash
                 let mut chave = caminho_imagem.as_bytes().to_vec();
                 chave.extend(data.timestamp_millis().to_be_bytes().iter());
@@ -93,15 +119,30 @@ fn main() {
                 let hash = blake3::hash(&chave);
 
                 // Gera o caminho do arquivo
-                let nome_imagem = format!("{}/{}.{}", &caminho_conversao, hash.to_hex(), configuracoes.extensao);
+                let nome_imagem = format!(
+                    "{}/{}.{}",
+                    &caminho_conversao,
+                    hash.to_hex(),
+                    configuracoes.extensao
+                );
 
                 // Salvar imagem
                 save_image(img, &nome_imagem);
-                green_ln!("Imagem {} salva em {}.", caminho_imagem, nome_imagem);
+                println!(
+                    "{}",
+                    style(format!(
+                        "Imagem {} salva em {}.",
+                        caminho_imagem, nome_imagem
+                    ))
+                    .green()
+                );
             });
         }
         Err(_) => {
-            red_ln!("Não foi possível criar a pasta de conversão.");
+            println!(
+                "{}",
+                style("Não foi possível criar a pasta de conversão.").red()
+            );
         }
     }
     pause();
@@ -111,7 +152,7 @@ fn pause() {
     let mut stdin = io::stdin();
     let mut stdout = io::stdout();
 
-    blue_ln!("\n\nFim.");
+    println!("{}", style("\n\nFim.").blue());
 
     // Queremos que o cursor fique no final da linha, então imprimimos sem uma linha nova
     write!(stdout, "Aperte enter para encerrar...").unwrap();
@@ -123,18 +164,20 @@ fn pause() {
 
 // Função que cria as configurações e carrega as configurações
 fn criar_configuracoes(caminho_configuracao: &str) -> Config {
-    
     /* Cria o objeto de configuração padrão, converte o objeto em string,
     cria o arquivo e salva as configurações transformando a string em bytes */
     let configuracoes = Config::default();
-    
+
     let conteudo = toml::to_string(&configuracoes).unwrap();
-    let mut arquivo = File::create(caminho_configuracao).expect("Não foi possível criar o arquivo de configuração.");
-    arquivo.write_all(conteudo.as_bytes()).expect("Não foi possível salvar o arquivo de configuração.");
+    let mut arquivo = File::create(caminho_configuracao)
+        .expect("Não foi possível criar o arquivo de configuração.");
+    arquivo
+        .write_all(conteudo.as_bytes())
+        .expect("Não foi possível salvar o arquivo de configuração.");
 
-    green_ln!("Arquivo de configurações criado.");
+    println!("{}", style("Arquivo de configurações criado.").green());
 
-    return configuracoes;
+    configuracoes
 }
 
 // Struct que cria e carrega arquivo de configuração
@@ -147,7 +190,7 @@ struct Config {
     canal_azul: i16,
     espelhamento_horizontal: bool,
     espelhamento_vertical: bool,
-    extensao: String
+    extensao: String,
 }
 
 // Implementa um valor padrão para inicializar a struct. Valor padrão de quando não houver configurações
@@ -161,7 +204,7 @@ impl Default for Config {
             canal_azul: 0,
             espelhamento_horizontal: false,
             espelhamento_vertical: false,
-            extensao: "jpg".to_string()
+            extensao: "jpg".to_string(),
         }
     }
 }
